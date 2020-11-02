748219f08c1beea03c8f509ad6504b06fc088df8
nguyentiendan <tiendanvn@gmail.com>
2020-11-02 21:31:48 +0900

    guide profile

---
+++ src-front-end/src/pages/guide.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+function User() {
+  return (
+    <Layout noHeader>
+      <h1>Guide Profile</h1>
+    </Layout>
+  );
+}
+export default User;
