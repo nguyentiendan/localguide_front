b2762a41e51347ad0fd680d2aa788723a3c0218f
odaira <odaira@stageinc.jp>
2021-08-23 15:20:47 +0900

    背景画像のカルーセル化：途中20210823

--- src-front-end/src/components/FadeBg/FadeBg.js
+++ src-front-end/src/components/FadeBg/FadeBg.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import Slider from 'react-slick';
-// import { baseUrl } from "./config";
-import styled from 'styled-components';
+import React from 'react';
+import Carousel from 'react-material-ui-carousel'
+import {Paper} from '@material-ui/core'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 
+/*
 class FadeBg extends Component {
   width100 = {
     width: '100%',
-
-    
   };
-
+*/
 /*
 // CSアニメーション用
   bg01 = {
@@ -35,42 +36,35 @@ class FadeBg extends Component {
 */
 
 
-
-  render() {
-    const settings = {
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 400,
-      dots: true,
-      fade: true,
-      infinite: true,
-      speed: 4000,
-      variableWidth: true,
-      centerMode: true,
-      centerPadding: 0,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-
-    return (
-      <div>
-        <Slider {...settings}>
-          <div style={this.width100}>
-            <img src={require('../../assets/img/bg2.jpg')} />
-          </div>
-          <div style={this.width100}>
-            <img src={require('../../assets/img/bg3.jpg')} />
-          </div>
-          <div style={this.width100}>
-            <img src={require('../../assets/img/bg4.jpg')} />
-          </div>
-          <div style={this.width100}>
-            <img src={require('../../assets/img/bg7.jpg')} />
-          </div>
-        </Slider>
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '380',
+    width: '100%',
+    background: 'black',
+    color: 'white',
+    textAlign: 'center',
+    lineHeight: '300px',
+    fontSize: '5rem'
+  },
+  table: {
+    minWidth: 650
   }
+}));
+
+function FadeBg() {
+  const classes = useStyles();
+  return (
+    <>
+      <Carousel animation="slide">
+        <div className={classes.root}><img src={require('../../assets/img/bg7.jpg')} /></div>
+        <div className={classes.root}><img src={require('../../assets/img/bg3.jpg')} /></div>
+        <div className={classes.root}><img src={require('../../assets/img/bg4.jpg')} /></div>
+        <div className={classes.root}><img src={require('../../assets/img/bg7.jpg')} /></div>
+      </Carousel>
+    </>
+  );
 }
+
+
 
 export default FadeBg;
--- src-front-end/src/components/Parallax/Parallax.js
+++ src-front-end/src/components/Parallax/Parallax.js
@@ -52,7 +52,7 @@ export default function Parallax(props) {
         transform,
       }}
     >
-      <FadeBg />
+    <FadeBg />
       {children}
     </div>
   );
