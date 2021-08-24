3f996d63fdb154ff0f97c5128bb76427224e71eb
c.sakamoto <sakamoto@stageinc.jp>
2021-08-24 10:22:46 +0900

    FadeBg componentの修正

--- src-front-end/src/components/FadeBg/FadeBg.js
+++ src-front-end/src/components/FadeBg/FadeBg.js
@@ -1,41 +1,10 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel';
-import { makeStyles } from '@material-ui/core';
-
-/*
-class FadeBg extends Component {
-  width100 = {
-    width: '100%',
-  };
-*/
-/*
-// CSアニメーション用
-  bg01 = {
-    width: '100%',
-    backgroundImage: "url(require('../../assets/img/bg2.jpg'))",
-    backgroundSize: 'cover',
-  };
-  bg02 = {
-    width: '100%',
-    backgroundImage: "url(require('../../assets/img/bg3.jpg'))",
-    backgroundSize: 'cover',
-  };
-  bg03 = {
-    width: '100%',
-    backgroundImage: "url(require('../../assets/img/bg4.jpg'))",
-    backgroundSize: 'cover',
-  };
-  bg04 = {
-    width: '100%',
-    backgroundImage: "url(require('../../assets/img/bg7.jpg'))",
-    backgroundSize: 'cover',
-  };
-*/
+import { makeStyles, Paper } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
     height: '90vh',
-    // maxHeight: "1000px",
     maxHeight: '765px',
     overflow: 'hidden',
     position: 'relative',
@@ -55,33 +24,22 @@ const useStyles = makeStyles(() => ({
 function FadeBg(props) {
   const { children } = props;
   const classes = useStyles();
+  const images = [
+    require('../../assets/img/bg2.jpg'),
+    require('../../assets/img/bg3.jpg'),
+    require('../../assets/img/bg4.jpg'),
+    require('../../assets/img/bg7.jpg'),
+  ];
   return (
     <>
       <Carousel className={classes.carousel} animation="slide">
-        <div
-          className={classes.root}
-          style={{ backgroundImage: `url(${require('../../assets/img/bg2.jpg')})` }}
-        >
-          {children}
-        </div>
-        <div
-          className={classes.root}
-          style={{ backgroundImage: `url(${require('../../assets/img/bg3.jpg')})` }}
-        >
-          {children}
-        </div>
-        <div
-          className={classes.root}
-          style={{ backgroundImage: `url(${require('../../assets/img/bg4.jpg')})` }}
-        >
-          {children}
-        </div>
-        <div
-          className={classes.root}
-          style={{ backgroundImage: `url(${require('../../assets/img/bg7.jpg')})` }}
-        >
-          {children}
-        </div>
+        {images.map(img => (
+          <Paper>
+            <div className={classes.root} style={{ backgroundImage: `url(${img})` }}>
+              {children}
+            </div>
+          </Paper>
+        ))}
       </Carousel>
     </>
   );
