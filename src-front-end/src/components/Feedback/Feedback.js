feb089fd568dc984278a693f734f94c7fbbc248a
c.sakamoto <sakamoto@stageinc.jp>
2021-09-06 15:53:57 +0900

    フィードバック表示位置の修正

--- src-front-end/src/components/Feedback/Feedback.js
+++ src-front-end/src/components/Feedback/Feedback.js
@@ -2,10 +2,33 @@ import React, { useState } from 'react';
 import { Modal, Button, Form, Input } from 'antd';
 import SendIcon from '@material-ui/icons/Send';
 import { makeStyles } from '@material-ui/core/styles';
+import { Box, Slide } from '@material-ui/core';
+import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
 import { container } from '../../assets/jss/material-kit-react';
 // import * as API from '../../apis';
 
 const styleWrapper = {
+  popup: {
+    position: 'absolute',
+    bottom: 0,
+    right: 10,
+    zIndex: 1000,
+    borderRadius: 3,
+    padding: '5px 20px',
+    backgroundColor: 'white',
+    boxShadow: '0px 0px 8px 2px lightgray',
+  },
+  popupButton: {
+    marginLeft: 'auto',
+    display: 'block',
+    position: 'absolute',
+    top: '-20px',
+    right: '20px',
+    color: 'gray',
+    borderRadius: '100px',
+    backgroundColor: 'white',
+    cursor: 'pointer',
+  },
   feedbackIcon: {
     textAlign: 'center',
     '@media (min-width: 576px)': {
@@ -42,13 +65,13 @@ const useStyles = makeStyles(styleWrapper);
 
 const Feedback = () => {
   const [form] = Form.useForm();
-  const [showModal, setShowModal] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const classes = useStyles();
 
   const handleCancel = () => {
-    setShowModal(false);
+    setShowPopup(false);
   };
 
   const onFinish = async values => {
@@ -61,7 +84,7 @@ const Feedback = () => {
       setErrorMessage('');
       // const { message, status } = await API.forgotPassword(values.title);
       setLoading(false);
-      setShowModal(false);
+      setShowPopup(false);
     } catch (error) {
       setLoading(false);
       setErrorMessage('An error has occurred.');
@@ -71,54 +94,55 @@ const Feedback = () => {
   return (
     <>
       <div className={`${classes.container} ${classes.feedbackIcon}`}>
-        <Button className={classes.feedbackButton} onClick={() => setShowModal(true)}>
+        <Button className={classes.feedbackButton} onClick={() => setShowPopup(true)}>
           Feedback
         </Button>
       </div>
 
-      <Modal
-        visible={showModal}
-        closable="false"
-        onCancel={handleCancel}
-        centered="true"
-        footer={null}
-      >
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={onFinish}
-          scrollToFirstError
-          requiredMark={false}
-        >
-          <div className={classes.errorMessage}>{errorMessage}</div>
-          <Form.Item
-            name="title"
-            label={<h3>Was this doc helpful to you?</h3>}
-            rules={[
-              {
-                required: true,
-                message: 'Please input comment!',
-              },
-              { max: 100 },
-            ]}
+      <Slide direction="up" in={showPopup} mountOnEnter unmountOnExit>
+        <Box className={classes.popup}>
+          <CancelOutlinedIcon
+            fontSize="large"
+            className={classes.popupButton}
+            onClick={() => setShowPopup(false)}
+          />
+          <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+            scrollToFirstError
+            requiredMark={false}
           >
-            <Input.TextArea size="large" allowClear autoSize={{ maxRows: 1}} />
-          </Form.Item>
-          <Form.Item name="comment" label={<p>your comment (optional)</p>} rules={[{ max: 200 }]}>
-            <Input.TextArea size="large" allowClear autoSize={{ maxRows: 5, minRows: 5 }} />
-          </Form.Item>
-          <Form.Item className={classes.form}>
-            <Button key="back" onClick={handleCancel} className={classes.formButton}>
-              Cancel
-            </Button>
-            <Button key="submit" type="primary" htmlType="submit">
-              <span>
-                Send feedback <SendIcon className={classes.formIcon} />
-              </span>
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
+            <div className={classes.errorMessage}>{errorMessage}</div>
+            <Form.Item
+              name="title"
+              label={<h3>Was this doc helpful to you?</h3>}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input comment!',
+                },
+                { max: 100 },
+              ]}
+            >
+              <Input.TextArea size="large" allowClear autoSize={{ maxRows: 1 }} />
+            </Form.Item>
+            <Form.Item name="comment" label={<p>your comment (optional)</p>} rules={[{ max: 200 }]}>
+              <Input.TextArea size="large" allowClear autoSize={{ maxRows: 5, minRows: 5 }} />
+            </Form.Item>
+            <Form.Item className={classes.form}>
+              <Button key="back" onClick={handleCancel} className={classes.formButton}>
+                Cancel
+              </Button>
+              <Button key="submit" type="primary" htmlType="submit">
+                <span>
+                  Send feedback <SendIcon className={classes.formIcon} />
+                </span>
+              </Button>
+            </Form.Item>
+          </Form>
+        </Box>
+      </Slide>
     </>
   );
 };
