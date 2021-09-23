2e84c542c0af0b853fa9d4edb445617be49fe7e7
nguyentiendan <tiendanvn@gmail.com>
2021-09-23 14:29:11 +0900

    setting master

--- src-front-end/src/pages/app.js
+++ src-front-end/src/pages/app.js
@@ -9,6 +9,9 @@ import AdminGuideReview from '../components/Admin/AdminGuideReview';
 import AdminUserReview from '../components/Admin/AdminUserReview';
 import AdminChangePass from '../components/Admin/AdminChangePass';
 import AdminSetInterest from '../components/Admin/AdminSetInterest';
+import AdminSetExtra from '../components/Admin/AdminSetExtra';
+import AdminSetTag from '../components/Admin/AdminSetTag';
+import AdminSetLanguage from '../components/Admin/AdminSetLanguage';
 
 // Guide
 import GuideAdmin from '../components/Guide/GuideAdmin';
@@ -45,6 +48,9 @@ const App = () => (
     <PrivateRoute path="/adminUserReview" component={AdminUserReview} />
     <PrivateRoute path="/adminChangePass" component={AdminChangePass} />
     <PrivateRoute path="/adminSetInterest" component={AdminSetInterest} />
+    <PrivateRoute path="/adminSetExtra" component={AdminSetExtra} />
+    <PrivateRoute path="/adminSetTag" component={AdminSetTag} />
+    <PrivateRoute path="/adminSetLanguage" component={AdminSetLanguage} />
 
     {/* Guide */}
     <PrivateRoute path="/guideAdmin" component={GuideAdmin} />
